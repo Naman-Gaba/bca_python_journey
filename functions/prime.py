319279c7e6796aecc400728168a7cdfe7ae49367
Naman <namangaba95@gmail.com>
2025-09-02 02:08:22 +0530

    Added Strings folder and updated loops/functions

--- functions/prime.py
+++ functions/prime.py
@@ -13,3 +13,16 @@ def prime(num):
 
 prime(17)  # prime
 prime(12)  # not prime
+
+##def prime(num):
+#    if num < 2:
+     #   print("not prime")
+    #    return  # stop here if <2
+   # 
+  #  for i in range(2, num):
+    #    if num % i == 0:
+   #         print("not prime")
+  #          return  # stop as soon as one divisor is found
+ #   
+#    print("prime", num)
+
--- loops/sum_of_digits.py
+++ loops/sum_of_digits.py
@@ -7,7 +7,6 @@ while num>0:
 print(sum)
 #wth for loop
 sum1=0
-
 for i in range(0,num):
     dig1=num%10
     sum1=sum1+dig1
